efb0d3a0a2f32f88e9f1c707fd35bc9b46202eb9
Braden <108199884+bradenr402@users.noreply.github.com>
2023-08-07 17:53:23 -0400

    Project complete

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,60 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for element in self
+      yield element
+    end
+    return self
+  end
+
+  def my_each_with_index
+    i = 0
+    for element in self
+      yield element, i
+      i += 1
+    end
+  end
+
+  def my_select
+    result = []
+    my_each { |element| result << element if yield element }
+    result
+  end
+
+  def my_all?
+    my_each { |element| return false unless yield element }
+    true
+  end
+
+  def my_any?
+    my_each { |element| return true if yield element }
+    false
+  end
+
+  def my_none?
+    my_each { |element| return false if yield element }
+    true
+  end
+
+  def my_count
+    return length unless block_given?
+    count = 0
+    my_each do |element|
+      count += 1 if yield element
+    end
+    count
+  end
+
+  def my_map block = nil
+    result = []
+    my_each { |element| result << (block ? block.call(element) : yield(element) ) }
+    result
+  end
+
+  def my_inject(initial = nil)
+    result = initial
+    my_each { |element| result = yield(result, element) }
+    result
+  end
 end
